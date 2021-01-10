e30bdae53c7787efacb7bb57a3ff1fa385b70222
LukeBenson <luke_benson@outlook.com>
2021-01-10 14:03:21 +0000

    first version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # discord-bot
+
+## Basic Use
+
+To use this bot, you will first need to [set up your own bot](https://realpython.com/how-to-make-a-discord-bot-python/).
+
+You will then need to use the **bot token** to set an environment variable `BOT_TOKEN`.
+
+> Note: this is a work-in-progress.
--- bennys-bot.py
+++ bennys-bot.py
@@ -1,7 +1,8 @@
 import discord
 import asyncio
 import time
-import request
+import requests
+import os
 
 client = discord.Client()
 
@@ -12,15 +13,25 @@ async def on_ready():
     print(f"We have logged in as {client.user}")
 
 @client.event
+async def on_member_join(member):
+    try:
+        role = member.guild.get_role(int(os.getenv("CANNON_FODDER")))
+        await member.add_roles(role)
+    except:
+        print(f"Could not give the role {role} to {member}")
+
+@client.event
 async def on_message(message):
     print(f"{message.author.id}")
-    if "@everyone" in message.content.lower():
-        insult = request.get("https://insult.mattbas.org/api/insult")
-	async with message.channel.typing():
-	    await asyncio.sleep(3)
-	await message.channel.send(f"{insult})
+    if message.mention_everyone:
+        insult = requests.get("https://insult.mattbas.org/api/insult")
+        hashtag = str(message.author).find("#")
+        person = str(message.author)[0:hashtag]
+        async with message.channel.typing():
+            await asyncio.sleep(3)
+        await message.channel.send(f"{insult.text}, {person}")
 
-    elif str(message.author) == "bengo#4992":
+    elif str(message.author) == os.getenv("HATED_USER"):
         time.sleep(1)
         with open("bengo.txt", "a") as file:
             text = f"Message: {message.content}. Time(UTC): {message.created_at} \r\n"
@@ -34,15 +45,27 @@ async def on_message(message):
         user_name = message.content.replace("bennybot.mute(", "")
         user_name = user_name.replace(")", "")
         user = message.guild.get_member_named(user_name)
-        await user.edit(mute=True)
-        await message.channel.delete()
+        try:
+            await user.edit(mute=True)
+        except:
+            async with message.channel.typing():
+                await asyncio.sleep(3)
+            await message.channel.send(f"Oops, it seems that command didn't work, let me go ahead and clean it up for you.")
+            print(f"Could not mute user {user}.")
+        await message.delete()
 
     elif "bennybot.unmute(" in message.content.lower():
         user_name = message.content.replace("bennybot.mute(", "")
         user_name = user_name.replace(")", "")
         user = message.guild.get_member_named(user_name)
-        await user.edit(mute=False)
-        await message.channel.delete()
+        try:
+            await user.edit(mute=True)
+        except:
+            async with message.channel.typing():
+                await asyncio.sleep(3)
+            await message.channel.send(f"Oops, it seems that command didn't work, let me go ahead and clean it up for you.")
+            print(f"Could not unmute user {user}.")
+        await message.delete()
 
     elif "bennybot.get_messages(bengo)" == message.content.lower():
         with open("bengo.txt", "r") as file:
@@ -52,7 +75,22 @@ async def on_message(message):
                 await asyncio.sleep(3)
             await message.channel.send("\r\n".join(text))
 
+    elif message.content.lower().startswith("bennybot.set_hated_user("):
+        user_name = message.content.replace("bennybot.mute(", "")
+        user_name = user_name.replace(")", "")
+        os.environ("HATED_USER") = user_name
+        message.delete()
+    
+    elif message.content.lower() == "bennybot.unset_hated_user()":
+        os.environ("HATED_USER") = ""
+        message.delete()
+
     elif "bennybot.logout()" == message.content.lower():
         await client.close()
 
-client.run("")
+    elif str(message.content).startswith("bennybot."):
+        async with message.channel.typing():
+            await asyncio.sleep(3)
+        await message.channel.send(f"Oops, it seems that isn't a valid command, please try a different command.")
+
+client.run(os.getenv('BOT_TOKEN'))
